2f4551d429456c1229ad7fb324a25f17e53dbb72
shu <shu@local>
2026-06-25 20:22:20 +0900

    Lay out game basics in one row

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "kusayakyu-log-v58";
+const CACHE_NAME = "kusayakyu-log-v59";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=43",
+  "./styles.css?v=44",
   "./app.js?v=50",
   "./manifest.webmanifest",
   "./icon.svg",
