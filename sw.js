c982a95936d996fd6fede0b42fb0fd66e92c8a7d
shu <shu@local>
2026-06-25 23:22:27 +0900

    Lay out game player fields in one row

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "kusayakyu-log-v61";
+const CACHE_NAME = "kusayakyu-log-v62";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=46",
+  "./styles.css?v=47",
   "./app.js?v=50",
   "./manifest.webmanifest",
   "./icon.svg",
