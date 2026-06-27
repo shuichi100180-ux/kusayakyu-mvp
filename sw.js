911eaafed2f2c5cf4a26a06d094a69a29872421e
shu <shu@local>
2026-06-27 17:14:23 +0900

    Keep mobile OPS note on one line

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "kusayakyu-log-v83";
+const CACHE_NAME = "kusayakyu-log-v84";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=65",
+  "./styles.css?v=66",
   "./app.js?v=64",
   "./manifest.webmanifest",
   "./icon.svg",
