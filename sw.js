6471cc7796636fb7f6333cfadb409f49919ff7d3
shu <shu@local>
2026-06-27 17:28:50 +0900

    Show mobile result buttons in four columns

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "kusayakyu-log-v87";
+const CACHE_NAME = "kusayakyu-log-v88";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=68",
+  "./styles.css?v=69",
   "./app.js?v=65",
   "./manifest.webmanifest",
   "./icon.svg",
