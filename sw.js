08757b1d944ecfcadf8b85f5c5ba6121c80091dd
shu <shu@local>
2026-06-28 19:28:22 +0900

    Tighten mobile game registration layout

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "kusayakyu-log-v108";
+const CACHE_NAME = "kusayakyu-log-v109";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=87",
+  "./styles.css?v=88",
   "./app.js?v=75",
   "./manifest.webmanifest",
   "./icon.svg",
