db3f9480e7c61a87e0eaf9930e140b8d6c9625d3
shu <shu@local>
2026-07-11 20:51:33 +0900

    Simplify batting summary labels

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "kusayakyu-log-v125";
+const CACHE_NAME = "kusayakyu-log-v126";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=99",
-  "./app.js?v=87",
+  "./app.js?v=88",
   "./manifest.webmanifest",
   "./icon.svg",
 ];
